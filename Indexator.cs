5baf988708608d2d99b040ac039fd3773d25cc2b
lehakarlov <lehakarlov@mail.ru>
2022-06-24 08:43:41 +0300

    24062022

--- Indexator.cs
+++ Indexator.cs
@@ -33,7 +33,7 @@ namespace MyRecipes
                     }
                     else
                     {
-                        //
+
                         return "error";
                     }
                 }
